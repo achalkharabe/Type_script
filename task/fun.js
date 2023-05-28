d0e16d6950695a78c0ccb493fee6c95c3fdcfbc9
achalkharabe <achalkharabe1@gmail.com>
2023-05-28 20:44:28 +0530

    commit task

---
+++ task/fun.js
@@ -0,0 +1,24 @@
+var add = function (a, b) {
+    var x = a + b;
+    return x;
+};
+var returnvalue = add(12, 3);
+console.log(returnvalue);
+var sub = function (a, b) {
+    var x = a - b;
+    return x;
+};
+var returnsub = sub(12, 3);
+console.log(returnsub);
+var multy = function (a, b) {
+    var x = a * b;
+    return x;
+};
+var returnmul = multy(12, 3);
+console.log(returnmul);
+var div = function (a, b) {
+    var x = a / b;
+    return x;
+};
+var returndiv = div(12, 2);
+console.log(returndiv);
---
+++ task/fun.ts
@@ -0,0 +1,31 @@
+const add=(a:number,b:number)=>{
+   const x:number=a+b;
+return x;
+}
+const returnvalue:number=add(12,3);
+console.log(returnvalue);
+
+
+
+const sub=(a:number,b:number)=>{
+    const x:number=a-b;
+    return x;
+}
+const returnsub:number=sub(12,3);
+console.log(returnsub);
+
+
+const multy=(a:number,b:number)=>{
+    const x:number=a*b;
+    return x;
+}
+const returnmul:number=multy(12,3);
+console.log(returnmul);
+
+
+const div=(a:number,b:number)=>{
+    const x:number=a/b;
+    return x;
+}
+const returndiv:number=div(12,2);
+console.log(returndiv);
